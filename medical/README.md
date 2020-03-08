a7d2c9d64c01c3215988ecc22c246055dd05af9b
guramritpalsaggu@gmail.com <guramritpalsaggu@gmail.com>
2020-03-09 03:52:42 +0530

    Initial

---
+++ medical/README.md
@@ -0,0 +1,17 @@
+# keras-vision-app
+
+Production ready starter pack for creating fast responsive Web App for Keras Image models using Starlette.io framework with Uvicorn ASGI server.
+
+Everything packaged in docker with requirement.txt, so you can push it to any docker hosted cloud service. Enjoy :)
+
+Few dockers hosted services where this starter pack should work =>
+
+* https://render.com
+* https://zeit.co/now
+* https://azure.microsoft.com/en-us/services/app-service/containers/
+* https://getcarina.com/
+* https://sloppy.io/en/
+* https://giantswarm.io
+* https://aws.amazon.com/ecs/
+* https://cloud.google.com/cloud-build/docs/
+* https://www.digitalocean.com/products/one-click-apps/docker/
---
+++ medical/app/server.py
@@ -0,0 +1,69 @@
+from keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from keras.models import load_model
+from keras.preprocessing import image
+from keras.applications.resnet50 import ResNet50
+from starlette.applications import Starlette
+from starlette.responses import HTMLResponse
+from starlette.staticfiles import StaticFiles
+from starlette.middleware.cors import CORSMiddleware
+from pathlib import Path
+import uvicorn, aiohttp, asyncio
+import base64, sys, numpy as np
+
+path = Path(__file__).parent
+model_file_url = 'VGG16_cats_and_dogs.h5' #DIRECT / RAW DOWNLOAD URL HERE!'
+model_file_name = 'model'
+
+app = Starlette()
+app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
+app.mount('/static', StaticFiles(directory='app/static'))
+
+MODEL_PATH = path/'models'/f'{model_file_name}.h5'
+IMG_FILE_SRC = '/tmp/saved_image.png'
+
+async def download_file(url, dest):
+    if dest.exists(): return
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            data = await response.read()
+            with open(dest, 'wb') as f: f.write(data)
+
+async def setup_model():
+    #UNCOMMENT HERE FOR CUSTOM TRAINED MODEL
+    # await download_file(model_file_url, MODEL_PATH)
+    # model = load_model(MODEL_PATH) # Load your Custom trained model
+    # model._make_predict_function()
+    model = ResNet50(weights='imagenet') # COMMENT, IF you have Custom trained model
+    return model
+
+# Asynchronous Steps
+loop = asyncio.get_event_loop()
+tasks = [asyncio.ensure_future(setup_model())]
+model = loop.run_until_complete(asyncio.gather(*tasks))[0]
+loop.close()
+
+@app.route("/upload", methods=["POST"])
+async def upload(request):
+    data = await request.form()
+    img_bytes = await (data["img"].read())
+    bytes = base64.b64decode(img_bytes)
+    with open(IMG_FILE_SRC, 'wb') as f: f.write(bytes)
+    return model_predict(IMG_FILE_SRC, model)
+
+def model_predict(img_path, model):
+    result = []; img = image.load_img(img_path, target_size=(224, 224))
+    x = preprocess_input(np.expand_dims(image.img_to_array(img), axis=0))
+    predictions = decode_predictions(model.predict(x), top=3)[0] # Get Top-3 Accuracy
+    for p in predictions: _,label,accuracy = p; result.append((label,accuracy))
+    result_html1 = path/'static'/'result1.html'
+    result_html2 = path/'static'/'result2.html'
+    result_html = str(result_html1.open().read() +str(result) + result_html2.open().read())
+    return HTMLResponse(result_html)
+
+@app.route("/")
+def form(request):
+    index_html = path/'static'/'index.html'
+    return HTMLResponse(index_html.open().read())
+
+if __name__ == "__main__":
+    if "serve" in sys.argv: uvicorn.run(app, host="0.0.0.0", port=8080)
