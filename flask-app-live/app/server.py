3633ea42eaf48f1c5c161d42ad3ef8911d687fcc
guramritpalsaggu@gmail.com <guramritpalsaggu@gmail.com>
2020-03-09 17:11:42 +0530

    Deploy

--- flask-app-live/app/server.py
+++ flask-app-live/app/server.py
@@ -30,10 +30,10 @@ async def download_file(url, dest):
 
 async def setup_model():
     #UNCOMMENT HERE FOR CUSTOM TRAINED MODEL
-    # await download_file(model_file_url, MODEL_PATH)
-    # model = load_model(MODEL_PATH) # Load your Custom trained model
-    # model._make_predict_function()
-    model = ResNet50(weights='imagenet') # COMMENT, IF you have Custom trained model
+    await download_file(model_file_url, MODEL_PATH)
+    model = load_model(MODEL_PATH) # Load your Custom trained model
+    model._make_predict_function()
+    # model = ResNet50(weights='imagenet') # COMMENT, IF you have Custom trained model
     return model
 
 # Asynchronous Steps
