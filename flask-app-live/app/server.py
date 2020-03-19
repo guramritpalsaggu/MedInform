3d597eb58eef2854b32d5e024a837998f364878f
gupta-keshav <36266680+gupta-keshav@users.noreply.github.com>
2020-03-20 00:00:19 +0530

    introduced new img_file_src

--- flask-app-live/app/server.py
+++ flask-app-live/app/server.py
@@ -21,7 +21,8 @@ app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Reques
 app.mount('/static', StaticFiles(directory='app/static'))
 
 MODEL_PATH = path/'models'/f'{model_file_name}.h5'
-IMG_FILE_SRC = path/'static'/'saved_image.png'
+# IMG_FILE_SRC = path/'static'/'saved_image.png'
+IMG_FILE_SRC = 'static/saved_image.png'
 PREDICTION_FILE_SRC = path/'static'/'predictions.txt'
 
 async def download_file(url, dest):
