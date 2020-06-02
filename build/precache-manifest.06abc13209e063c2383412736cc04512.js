1cb3be5cf9a0a48a465bcfb68249d60ed09d74e9
guramritpalsaggu@gmail.com <guramritpalsaggu@gmail.com>
2020-06-03 01:36:18 +0530

    update

---
+++ build/precache-manifest.06abc13209e063c2383412736cc04512.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4a0ee0ade23f080450bef556a90efe8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5bd943b4308197b17fad",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "9db44b7e3b06efbe8272",
+    "url": "/static/css/main.3bfde885.chunk.css"
+  },
+  {
+    "revision": "5bd943b4308197b17fad",
+    "url": "/static/js/2.277763a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.277763a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9db44b7e3b06efbe8272",
+    "url": "/static/js/main.cfe8398f.chunk.js"
+  },
+  {
+    "revision": "3476eaf0b11ab002362c",
+    "url": "/static/js/runtime-main.abc6811d.js"
+  },
+  {
+    "revision": "1e9efde8de66b10b0cccb01bfa95b4ba",
+    "url": "/static/media/Malaria.1e9efde8.jpg"
+  },
+  {
+    "revision": "9370e78cea6eb124ec62e95cd96d4f4a",
+    "url": "/static/media/Pneumonia.9370e78c.jpg"
+  },
+  {
+    "revision": "053c0011cfe5df5c94f02e048d49824e",
+    "url": "/static/media/Tuberculosis.053c0011.jpg"
+  },
+  {
+    "revision": "488056b11d44c944b12aa27d561bb7a5",
+    "url": "/static/media/bg1.488056b1.jpg"
+  },
+  {
+    "revision": "c063a010b1bddbb0d1e855fe1d04f8ae",
+    "url": "/static/media/illu3.c063a010.jpg"
+  },
+  {
+    "revision": "840a5b7e560ca6a7e85dd82803393277",
+    "url": "/static/media/logo.840a5b7e.jpeg"
+  },
+  {
+    "revision": "c7683dbca1beffc24423e448e2c77ffb",
+    "url": "/static/media/retinopathy.c7683dbc.jpg"
+  },
+  {
+    "revision": "774f90923af458cd058ecf6555ba9190",
+    "url": "/static/media/skin.774f9092.jpg"
+  }
+]);
